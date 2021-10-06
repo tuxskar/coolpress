b887861266ec84d941c52f4a803df6ebcdcf2c14
Oscar Ramirez <tuxskar@gmail.com>
2021-10-06 18:56:28 +0200

    Added post detail view

--- coolpress/press/urls.py
+++ coolpress/press/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='posts_list'),
+    path('post/<int:post_id>/', views.post_detail, name='posts_detail'),
 ]
--- coolpress/press/views.py
+++ coolpress/press/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from press.models import PostStatus, Post
 
@@ -6,3 +6,8 @@ from press.models import PostStatus, Post
 def post_list(request):
     post_list = Post.objects.filter(status=PostStatus.PUBLISHED).order_by('last_update')[:15]
     return render(request, 'posts_list.html', {'post_list': post_list})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'posts_detail.html', {'post_obj': post})
