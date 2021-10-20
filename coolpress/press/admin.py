1c86111206b62a04be58f0c59ec5bc6ca14a0df8
Oscar Ramirez <tuxskar@gmail.com>
2021-10-20 19:11:20 +0200

    Added the Admin models registered

--- coolpress/press/admin.py
+++ coolpress/press/admin.py
@@ -1,3 +1,72 @@
-from django.contrib import admin
+from urllib.parse import urlencode
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.html import format_html
+
+from press.models import CoolUser, Category, Post
+
+
+class CoolUserInline(admin.StackedInline):
+    model = CoolUser
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (CoolUserInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+class CoolUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'user', 'github_profile', 'view_post_link', 'gh_repositories', 'gravatar_link')
+    list_filter = ('gh_repositories',)
+
+    def view_post_link(self, obj):
+        count = obj.post_set.count()
+        url = (
+                reverse("admin:press_post_changelist")
+                + "?"
+                + urlencode({"author__id": f"{obj.id}"})
+        )
+        label = f'Posts' if count > 1 else 'Post'
+        return format_html(f'<a href="{url}">{count} {label}</a>', url, count)
+
+    view_post_link.short_description = "Post"
+
+
+admin.site.register(CoolUser, CoolUserAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'label', 'view_post_link')
+
+    def view_post_link(self, obj):
+        count = obj.post_set.count()
+        url = (
+                reverse("admin:press_post_changelist")
+                + "?"
+                + urlencode({"category__id": f"{obj.id}"})
+        )
+        label = f'Posts' if count > 1 else 'Post'
+        return format_html(f'<a href="{url}">{count} {label}</a>', url, count)
+
+    view_post_link.short_description = "Post"
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'creation_date'
+    list_filter = ('category', 'status')
+    search_fields = ("title", "body")
+    list_display = ('title', 'author', 'last_update', 'status', 'category', 'image_link')
+
+
+admin.site.register(Post, PostAdmin)
