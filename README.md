e65bae850b36f8a13c1c4ac5af8360c40b08a4d1
Oscar Ramirez <tuxskar@gmail.com>
2021-10-22 13:21:17 +0200

    Added posts detail html and redirecting index to posts_list

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 CoolPress is an application to show the power of web development using Django
 
 Hey It's **Oscar Ramirez**
+
+If the debug breakpoints doesn't work check if the support for GEVENT COMPATIBLE is checked, and uncheck that option on pycharm!
--- coolpress/press/urls.py
+++ coolpress/press/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.post_list, name='index'),
     path('post/<int:post_id>', views.post_detail, name='posts-detail'),
     path('posts/', views.post_list, name='posts-list'),
 ]
