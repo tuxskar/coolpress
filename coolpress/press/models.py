368b6316c72a335e10fb017a495ad731c218066c
Oscar Ramirez <tuxskar@gmail.com>
2022-11-09 13:00:32 +0100

    Added the models

--- coolpress/press/models.py
+++ coolpress/press/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class CoolUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gravatar_link = models.URLField(null=True, blank=True)
+    github_profile = models.URLField(null=True, blank=True)
+    gh_repositories = models.IntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.user.username}'
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    label = models.CharField(max_length=200)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return f'{self.label} ({self.id})'
+
+
+class PostStatus:
+    DRAFT = 'DRAFT'
+    PUBLISHED = 'PUBLISHED'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=400)
+    body = models.TextField(null=True)
+    image_link = models.URLField(null=True)
+    status = models.CharField(max_length=32,
+                              choices=[(PostStatus.DRAFT, 'Draft'),
+                                       (PostStatus.PUBLISHED, 'Pasdfasdfublished Post')],
+                              default=PostStatus.DRAFT)
+
+    author = models.ForeignKey(CoolUser, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
