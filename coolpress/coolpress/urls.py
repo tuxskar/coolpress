ee0b6accf066818c3387a6e1ae22dcb316608b85
F6ew <fatmaalrashdi@gmail.com>
2022-11-17 22:48:12 +0100

    update urls

--- coolpress/coolpress/urls.py
+++ coolpress/coolpress/urls.py
@@ -9,8 +9,6 @@ router.register(r'posts', views.PostViewSet)
 router.register(r'authors', views.AuthorsViewSet)
 
 urlpatterns = [
-    path('home/', views.home),
-    path('posts-old/', views.posts_list, name='posts-list'),
     path('post_details/<int:post_id>', views.post_detail, name='posts-detail'),
     path('post/<int:post_id>/comment-add/', views.add_post_comment, name='comment-add'),
     path('post/update/<int:post_id>', views.post_update, name='post-update'),
@@ -18,12 +16,16 @@ urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
     path('categories/', views.CategoryListView.as_view(), name='category-list'),
     path('posts/', views.PostClassBasedListView.as_view(), name='post-list'),
+    path('posts/<str:category_slug>', views.PostClassFilteringListView.as_view(),
+         name='posts-list-by-category'),
     path('posts/<slug:category_slug>', views.PostClassFilteringListView.as_view(), name='post-list-filtered-by-category'),
     path('api-category/<slug:slug>', views.category_api, name='category-api'),
-
     path('api-categories/', views.categories_api, name='categories-api'),
-
     path('api/', include(router.urls)),
+    path('post-search/', views.search_post, name='post-search'),
     path('api-auth/', include('rest_framework.urls')),
+    path('login/', views.signup, name='login'),
+    path('test-email/', views.test_send_email),
+    path('signup/', views.signup, name='signup'),
 
 ]
